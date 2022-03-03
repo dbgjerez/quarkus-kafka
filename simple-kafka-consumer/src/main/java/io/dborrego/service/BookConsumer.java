975c5e94c49d3a053bfc45e4becdfd275302ba76
David Borrego <dborrego@redhat.com>
2022-03-03 01:44:03 +0100

    sleep posibility for demos

--- simple-kafka-consumer/src/main/java/io/dborrego/service/BookConsumer.java
+++ simple-kafka-consumer/src/main/java/io/dborrego/service/BookConsumer.java
@@ -1,7 +1,10 @@
 package io.dborrego.service;
 
+import java.util.Optional;
+
 import javax.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 
@@ -10,9 +13,24 @@ public class BookConsumer {
 
     private static final Logger log = Logger.getLogger(BookConsumer.class);
 
+    @ConfigProperty(name = "mp.messaging.incoming.books-new.lag")
+    Optional<Long> lag;
+
     @Incoming("books-new")
     public void toUpperCase(String message) {
+        lag.ifPresent(l -> {
+            log.info("Sleeping: " + l.toString());
+            sleep(l);
+        });
         log.info(message);
+    }
+
+    private void sleep(Long l) {
+        try {
+            Thread.sleep(l);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
     }
 
 }
