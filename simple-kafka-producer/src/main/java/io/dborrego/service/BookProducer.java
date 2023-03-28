210b1456cab822fba12dc8db86be1dfb4924d142
David Borrego <dborrego@redhat.com>
2023-03-28 12:17:05 +0200

    parameters

--- simple-kafka-producer/src/main/java/io/dborrego/service/BookProducer.java
+++ simple-kafka-producer/src/main/java/io/dborrego/service/BookProducer.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import io.smallrye.mutiny.Multi;
@@ -16,12 +17,19 @@ public class BookProducer {
     // Pages contains the seconds of the msg is sended
     private static final String BOOK_FORMAT_STRING = "Book[ title: {0}, Pages: {1} ]";
 
+    @ConfigProperty(name = "seconds")
+    Integer seconds;
+
     @Outgoing("books-new")
     public Multi<String> generate() {
-        return Multi.createFrom().ticks().every(Duration.ofSeconds(1)).map(generateBook);
+        return Multi.createFrom()
+                .ticks()
+                .every(Duration.ofSeconds(seconds))
+                .map(generateBook);
     }
 
-    Function<Long, String> generateBook = l -> MessageFormat.format(BOOK_FORMAT_STRING, "The Lord of the Rings",
+    Function<Long, String> generateBook = l -> MessageFormat.format(
+            BOOK_FORMAT_STRING, "The Lord of the Rings",
             String.valueOf(l));
 
 }
